41af67d3765fd4b6d1a2c45475d22b29242a57b2
Alex-Munezz <munezz456@gmail.com>
2023-07-06 08:44:59 +0300

    creating login page

--- barrel-and-bottle/src/App.js
+++ barrel-and-bottle/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import LiquorList from './liquorList';
 import './App.css';
 import {Twemoji} from 'react-emoji-render';
-
 function App() {
   return (
     <div className="App">
       <div className="h1">
         <h1>Welcome to Barrel & Bottle <Twemoji text="🥂" /></h1>
       </div>
-      <LiquorList />
     </div>
   );
 }
